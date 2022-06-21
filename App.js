a05a8f619bfe6be69c1220f9f77b3f6a75e52de4
moom1 <48647021+moom1@users.noreply.github.com>
2022-06-21 11:33:39 +0300

    First APK creation using EAS

--- App.js
+++ App.js
@@ -1,10 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image,StyleSheet, Text, View,TouchableOpacity, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing'; 
+import uploadToAnonymousFilesAsync from 'anonymous-files'; 
+
+import {useState} from "react"
+
+// import * as SplashScreen from 'expo-splash-screen';
+
+// SplashScreen.preventAutoHideAsync();
+// setTimeout(SplashScreen.hideAsync, 5000);
+
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    if (Platform.OS === 'web') {
+      let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri });
+    } else {
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri: null });
+    } 
+
+    console.log(pickerResult);
+  }
+
+  let openShareDialogAsync = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert(`The image is available for sharing at: ${selectedImage.remoteUri}`);
+      return;
+    }
+
+    Sharing.shareAsync(selectedImage.remoteUri || selectedImage.localUri);
+  }; 
+
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+
+        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+          <Text style={styles.buttonText}>Share this photo</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
+      <Text style={styles.instructions}>To share a photo from your phone with a friend, just press the button below!</Text>
+
+      <TouchableOpacity 
+        onPress={openImagePickerAsync}  style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
+
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +90,34 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  instructions:{
+    color: '#888',
+    fontSize: 19,
+    marginHorizontal: 15,
+
+  },
+
+  logo:{
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  },
+  
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
+  }
+
 });
